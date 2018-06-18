d8e33919097bcb198bf42c06aee3163738da5f52
osvaldo_trejo <osvaldo_trejo@osvaldoMr>
2018-06-18 16:51:37 -0500

    Actualizacion 2, prueba

--- src/ordenamiento/por/insercion/OrdenamientoPorInsercion.java
+++ src/ordenamiento/por/insercion/OrdenamientoPorInsercion.java
@@ -1,8 +1,5 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+//Ordenamiento de numeros por por insercion
+
 package ordenamiento.por.insercion;
 import java.util.Scanner;
 import javax.swing.JOptionPane;
